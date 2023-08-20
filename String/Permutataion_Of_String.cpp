1aa946e27465b16fdae827acad929ca5deb34eb0
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2023-08-20 15:15:23 +0530

    Update Permutataion_Of_String.cpp

--- String/Permutataion_Of_String.cpp
+++ String/Permutataion_Of_String.cpp
@@ -1,25 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void permutation(string &s, int left, int right){
-    if(left == right)
-        cout<<s<<endl;
+// Type 1: Storing all Permutations including duplicates
+void permutation(int left, int right, string &s, vector<string> &ans){
+    if (left == right){// reach the end of s
+        ans.push_back(s);
+        return ;
+    } 
 
-    else{
-        for (int i = left; i <= right ; i++){
-            swap(s[left], s[i]);
-            permutation(s,left+1,right);
-            swap(s[left], s[i]); // backtrack
-        }
+    for (int j=left; j<=right; j++){
+        swap(s[left], s[j]);
+        permutation(left+1, right, s, ans);
+        swap(s[left], s[j]);     // backtrack
     }
-    
 }
 
+vector<string> AllPerm(string &s){
+    vector<string> ans;
+    int n = s.size();
+    sort(s.begin(), s.end());
+    permutation(0,n-1,s,ans);
+
+    return ans; 
+}
+
+
+// Type 2: Storing  unique permutations 
+void FindUniquePerm(int left, int right, string &s, vector<string> &ans){
+    if (left == right){// reach the end of s
+        ans.push_back(s);          // if we use set<string> ans, then we need to insert instead of push_back
+        return ;
+    } 
+
+    FindUniquePerm(left+1, right, s, ans);    // if we use set<string> ans, then we don't need this call
+
+    for (int j=left+1; j<=right; j++){       // if we use set<string> ans, then we can start j from left
+        // if we use set<string> ans, then don't need following condition
+        if (s[left] != s[j]){        // checking for unique numbers
+            swap(s[left], s[j]);
+            FindUniquePerm(left+1, right, s, ans);
+            swap(s[left], s[j]);     // backtrack
+        } 
+    }
+}    
+
+vector<string> permuteUnique(string &s) {
+    vector<string> ans;
+    int n = s.size();
+    sort(s.begin(), s.end());
+    FindUniquePerm(0,n-1, s, ans);
+
+    // return vector<string>(ans.begin(), ans.end());     // if set<string> ans is used insted of vector
+
+    return ans;
+}
+
+
 int main(){
-    string str ="ABC";
+    string str ="AAC";
     int n = str.size();
 
-    permutation(str, 0, n-1);
-    return 0;
+    cout<<"Printing all Permutations of string : \n";
+    vector<string> perms = AllPerm(str);
+    for (int i = 0; i < perms.size(); i++){
+        cout<<perms[i]<<" ";
+    }
 
+    cout<<"\n\nPrinting unique Permutations of string : \n";
+    vector<string> uniquePerm = permuteUnique(str);
+    for (int i = 0; i < uniquePerm.size(); i++){
+        cout<<uniquePerm[i]<<" ";
+    }
+        
+    return 0;
 }
