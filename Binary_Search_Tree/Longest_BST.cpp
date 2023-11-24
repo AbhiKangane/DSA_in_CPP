27b84020f2463915a8563e027fd8998f347ccca7
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2023-11-24 19:59:31 +0530

    Update Largest_BST_Subtree.cpp

--- Binary_Search_Tree/Longest_BST.cpp
+++ Binary_Search_Tree/Longest_BST.cpp
@@ -1,3 +1,7 @@
+// GFG Problem:
+// Given a binary tree. Find the size of its largest subtree that is a Binary Search Tree.
+// Note: Here Size is equal to the number of nodes in the subtree.
+
 #include<iostream>
 #include<queue>
 using namespace std;
@@ -33,7 +37,7 @@ node* build_tree(node* root){  // traversal is a preorder
     return root;
 }
 
-// solution starts
+// solution 1 starts
 class info{
 public:
     int maxi, mini; 
@@ -68,12 +72,60 @@ info solve(node* root, int &ans){
         currNode;         
 }
 
-int LongestBST(node* root){
+int LargestBST(node* root){
     int maxSize = 0;
     info temp = solve(root, maxSize);
     return maxSize;  // no. of nodes in BST
 }
 // ends
+
+
+// solution 2 made by me starts here
+int isBSTUtil(node* root, int min, int max){
+    if (root == NULL)
+        return 1;
+    
+    if (root->data < min || root->data > max)
+        return 0;
+    
+    return isBSTUtil(root->left, min, root->data - 1)
+            && isBSTUtil(root->right, root->data + 1,max); 
+}
+// for check subtree is BST
+bool isBST(node* root) {
+    return (isBSTUtil(root, INT32_MIN, INT32_MAX));
+}
+
+// for counting no. of nodes in BST
+int countNodes(node* root){
+    //base case
+    if(root==NULL)
+        return 0;
+
+    //recursive call to left child and right child and
+    // add the result of these with 1 ( 1 for counting the root)
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+    
+int largestBst_Util(node* root){
+    if(root== NULL)
+        return 0;
+    
+    if(isBST(root))
+        return countNodes(root);
+    
+    int forleft = largestBst_Util(root->left);
+    int forright = largestBst_Util(root->right);
+    
+    return max(forleft, forright);
+}
+
+/*You are required to complete this method */
+// Return the size of the largest sub-tree which is also a BST
+int largestBst_2(node *root)
+{
+    return largestBst_Util(root);
+}
 
 
 int main(){
@@ -93,5 +145,6 @@ int main(){
     root = build_tree(root);
     // 3 10 5 2 -1 -1 8 -1 -1 15 12 -1 -1 17 -1 -1 5 -1 -1
 
-    cout<<"Size of longest BST in binary tree is: "<<LongestBST(root);
+    // cout<<"Size of longest BST in binary tree is: "<<LargestBST(root);
+    cout<<"Size of longest BST in binary tree is: "<<largestBst_2(root);
 }
