842dae1b8d824eaf0e8e0a1c3a2095998cc9efa4
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2023-12-17 17:55:22 +0530

    Update General_Operations.cpp

--- Bit_Manipulation/General_Operations.cpp
+++ Bit_Manipulation/General_Operations.cpp
@@ -20,10 +20,17 @@ int updateBit(int n, int pos, int value){
     return (n | (value<<pos));
 }
 
+int changeBits(int n){   // it changes bits value 1 -> 0 && 0 -> 1
+    int k = (1 << (int)floor(log2(n))+1) - 1;  // setting all bits to 1
+    return k^n; // as we know 1^1 = 0 && 0^1 = 1
+    // return k-n;  // ex. 15-10 = 15^10 , 31^24 = 31-24
+}
+
 int main(){
     cout<<getBit(5,2)<<endl; // indexing in bit starts from right->left & from 0   -> 5 = 0101
     cout<<setBit(5,1)<<endl; // sets bit value at index 1 - '1' : ans = 7  = 0111
     cout<<clearBit(5,0)<<endl; //sets bit value at 0th index - '0' : ans = 4 = 0100
     cout<<updateBit(5,1,1)<<endl; // updates bit value at given index 
+    cout<<changeBits(24)<<endl; // updates bit value at given index 
     return 0;
 }
