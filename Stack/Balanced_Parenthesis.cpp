43aca223600affc3fbd49f614213eaa834fb828b
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-01-30 12:21:46 +0530

    Update Balanced_Parenthesis.cpp

--- Stack/Balanced_Parenthesis.cpp
+++ Stack/Balanced_Parenthesis.cpp
@@ -4,51 +4,26 @@ using namespace std;
 
 bool check_Balance(string s){
     stack<char> st;
-    bool ans = true;
 
     for(int i=0; i<s.length(); i++){
         if(s[i] == '{' || s[i]=='(' || s[i]=='[')
             st.push(s[i]);
         
-        else if(s[i] == ')'){
-            if(!st.empty() && st.top() == '(')
-                st.pop();
-                
-            else{
-                ans = false;
-                break;
-            }
-        }
+        else{
+            if(s[i]=='}' && s[i]=='{' ||
+               s[i]==']' && s[i]=='[' ||
+               s[i]==')' && s[i]=='('
+            )   
+                return false;
 
-        else if(s[i] == ']'){
-            if(!st.empty() && st.top() == '[')
-                st.pop();
-                
-            else{
-                ans = false;
-                break;
-            }
-        }
-
-        else if(s[i] == '}'){
-            if(!st.empty() && st.top() == '{')
-                st.pop();
-                
-            else{
-                ans = false;
-                break;
-            }
+            st.pop();
         }
     }
-
-    if(!st.empty())
-        return false;
-    
-    return ans;
+    return st.empty();
 }
 
 int main(){
-    string s = "{([])}";
+    string s = "{([}])}";
     
     if(check_Balance(s))
         cout<<"Valid string\n";
