5255db69d72d6574014536b202729803cddeb760
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-01-31 12:04:26 +0530

    Update Trie_Implementation_leetcode_208.cpp

--- Trie/Implementation.cpp
+++ Trie/Implementation.cpp
@@ -1,3 +1,11 @@
+// leetcode 208: https://leetcode.com/problems/implement-trie-prefix-tree/
+/*
+Implement the Trie class:
+    1) Trie() Initializes the trie object.
+    2) void insert(String word) Inserts the string word into the trie.
+    3) boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+    4) boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+*/
 #include<iostream>
 using namespace std;
 
@@ -50,7 +58,7 @@ class Trie{
         Help_Insert(child, word.substr(1));
     }
 
-    void insert_Word(string word){
+    void insert(string word){
         Help_Insert(root, word);
     }
 
@@ -78,11 +86,27 @@ class Trie{
         return Help_Search(child, word.substr(1));
     }
 
-    bool Search_Word(string word){
+    bool search(string word){
         return Help_Search(root, word);
     }
-};
 
+    
+    bool Help_startsWith(TrieNode* root, string word)
+    {
+        if(word.length()==0) 
+            return true;
+        
+        int index=word[0]-'a';
+        
+        if(root->children[index]!=NULL)
+            return Help_startsWith(root->children[index],word.substr(1));
+        
+        return false;
+    }
+    bool startsWith(string prefix) {
+        return Help_startsWith(root, prefix);
+    }
+};
 int main(){
     Trie t;
     t.insert_Word("abcd");
