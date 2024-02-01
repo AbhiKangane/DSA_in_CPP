2e6cde31c330a3f50c0729278be7f12fb1c11b70
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-02-01 22:49:29 +0530

    Update Subset_Sum.cpp

--- DP/Subset_Sum.cpp
+++ DP/Subset_Sum.cpp
@@ -1,46 +1,109 @@
-// Here is the top-down approach of
-// dynamic programming
+// dynamic programming : Subset Sum Problem GFG
 #include <bits/stdc++.h>
 using namespace std;
  
-// function for checking that given array contains subsets of given target or not
-bool checkSubset(int arr[], int target, int n){
-    // creating 2d dp array
-    bool dp[n+1][target+1];
+// recursive sol for target sum
+bool solveRec(int index, vector<int> &arr, int size, int target){
+    // base case
+    if(index >= size || target < 0)
+        return 0;
+    if(target == 0)
+        return 1;
+
+    bool incl = solveRec(index+1, arr, size, target-arr[index]);
+    bool excl = solveRec(index+1, arr, size, target);
+
+    return incl || excl;
+}
+
+// recursive memoization sol for target sum
+bool solveMem(int index, vector<int> &arr, int size, int target, vector<vector<int>> &dp){
+    // base case
+    if(target == 0)
+        return dp[index][target] = 1;
+
+    if(index >= size || target < 0)
+        return 0;
+
     
-    for(int i=0; i<n+1; i++){
-        for(int j=0; j<target+1; j++){
-            if(i == 0 & j != 0)  // if given set( or arr) is empty
-                dp[i][j] = false;
+    if(dp[index][target] != -1)
+        return dp[index][target];
+
+    bool incl = solveMem(index+1, arr, size, target-arr[index], dp);
+    bool excl = solveMem(index+1, arr, size, target, dp);
+
+    return dp[index][target] = incl || excl;
+}
+
+// iterative tabulation sol for equal sum partition
+bool solveTab(vector<int> &arr, int size, int target){
+    vector<vector<int>> dp(size+1, vector<int>(target+1, 0));
+    // analysing base case
+    for (int i = 0; i<=size; i++){
+        dp[i][0] = 1;
+    }
+    
+    for (int index = size-1; index >= 0; index--){
+        for (int t = 0; t <= target; t++){
+            bool incl = 0;
+            if(t-arr[index] >= 0)
+                incl = dp[index+1][t-arr[index]];
             
-            if(j == 0)   // for target = 0 
-                dp[i][j] = true;
+            bool excl = dp[index+1][t];
 
-            else if(arr[i-1] <= j)
-                dp[i][j] = dp[i-1][j-arr[i-1]]/*number included*/ || dp[i-1][j] /*number not included*/;
-
-            else if(arr[i-1] > j) // if arr[i-1] is greater than target then number isn't included
-                dp[i][j] = dp[i-1][j];
+            dp[index][t] = incl || excl;
         }
     }
-
-    // for(int i=0; i<n+1; i++){
-    //     for(int j=0; j<target+1; j++)
-    //         cout<<dp[i][j]<<" ";
-    //     cout<<endl;
-    // }cout<<endl;
-
-
-    return dp[n][target];
+    
+    return dp[0][target];
 }
 
 
+// space optimization sol for equal sum partition
+bool solve_SO(vector<int> &arr, int size, int target){
+    vector<int> curr(target+1, 0);
+    vector<int> next(target+1, 0);
+    // analysing base case
+    curr[0] = 1;
+    next[0] = 1;
+    
+    for (int index = size-1; index >= 0; index--){
+        for (int t = 0; t <= target; t++){
+            bool incl = 0;
+            if(t-arr[index] >= 0)
+                incl = next[t-arr[index]];
+            
+            bool excl = next[t];
+
+            curr[t] = incl || excl;
+        }
+        next = curr;
+    }
+    
+    return next[target];
+}
+    
+bool isSubsetSum(vector<int>arr, int target){
+    int size = arr.size();
+    // sol 1
+    // return solveRec(0, arr, size, target);
+    
+    // sol 2
+    // vector<vector<int>> dp(size+1, vector<int>(target+1, -1));
+    // return solveMem(0, arr, size, target, dp);
+    
+    // sol 3
+    // return solveTab(arr, size, target);
+    
+    // sol 4
+    return solve_SO(arr, size, target);
+}
+
 int main()
 {
-    int arr[] = {1,1,1,1,1,4};
+    vector<int> arr = {1,1,1,1,1,4};
     int target = 9;
-    int size = sizeof(arr)/sizeof(arr[0]);
-
-    cout<<checkSubset(arr, target, size);
+    
+    cout<<isSubsetSum(arr, target);
     return 0;
 }
