83d8e16d92012ab668075216f2ebf01ad2a850e2
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-02-20 14:38:54 +0530

    Update Longest_Arithmetic_Subsequence.cpp

--- DP/Longest_Arithmetic_Subsequence.cpp
+++ DP/Longest_Arithmetic_Subsequence.cpp
@@ -1,8 +1,8 @@
-// gfg question
+// gfg question & leetcode 1027
 #include<bits/stdc++.h>
 using namespace std;
 
-// recursive sol
+// recursive sol : 169/220
 int solveRec(int index, int diff, int A[]){
     // backward check
     if(index < 0)
@@ -17,7 +17,8 @@ int solveRec(int index, int diff, int A[]){
     return ans;
 }
 
-// recursive memoization sol
+
+// recursive memoization sol : 126/220
 int solveMem(int index, int diff, int A[], unordered_map<int,int> dp[]){
     // backward check
     if(index < 0)
@@ -29,16 +30,20 @@ int solveMem(int index, int diff, int A[], unordered_map<int,int> dp[]){
     int ans = 0;
     for (int i = index-1; i>=0; i--){
         if(A[index] - A[i] == diff)
-            ans = max(ans, 1+solveRec(i, diff, A));
+            ans = max(ans, 1+solveMem(i, diff, A, dp));
     }
     
     return dp[index][diff] = ans;
 }
 
-// iterative tabulation
-int solveTab(int A[], int n){
+
+// sol 3: Tabulation :
+int solveTab(int A[], int n) {
+    if(n <= 2)
+        return n;
+    
     int ans = 0;
-    unordered_map<int,int> dp[n+1];
+    vector<vector<int>> dp(n+1, vector<int>(A[n-1] - A[0] +1, INT_MIN));
 
     for (int i = 1; i < n; i++){
         for(int j=0; j<i; j++){
@@ -46,7 +51,7 @@ int solveTab(int A[], int n){
             int cnt = 1;
 
             // check if answer already present 
-            if(dp[j].count(diff))
+            if(dp[j][diff] != INT_MIN)
                 cnt = dp[j][diff];
             
             dp[i][diff] = 1 + cnt;
@@ -56,31 +61,51 @@ int solveTab(int A[], int n){
     return ans;
 }
 
-int LengthOfLongestAP(int A[], int n){
+// driver function
+int lengthOfLongestAP(int A[], int n) {
+    // if(n <= 2)
+    // return n;
+
+    // int ans = 0;
+
+    // // for sol 2
+    // unordered_map<int,int> dp[n+1];
+
+    // for (int i = 0; i < n; i++){
+    //     for(int j=i+1; j<n; j++){
+            
+    //         ans = max(ans, 2+solveMem(i, A[j]-A[i],A,dp));
+    //     }
+    // }
+    // return ans;
+    
+    // sol 3
+    return solveTab(A, n);
+}
+
+
+// for leetcode solution
+int longestArithSeqLength(vector<int>& nums, int n) {
     if(n <= 2)
         return n;
     
-    int ans = 0;
+    int longest = 2;
+    vector<unordered_map<int, int>> dp(n);
 
-// for sol 2
-    unordered_map<int,int> dp[n+1];
-
-    for (int i = 0; i < n; i++){
-        for(int j=i+1; j<n; j++){
-// sol 1
-            // ans = max(ans, 2+solveRec(i, A[j]-A[i], A));
-// sol 2
-            ans = max(ans, 2+solveMem(i, A[j]-A[i],A,dp));
-            // ans = max(ans, 2+solveRec(i, A[j]-A[i], A));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            int diff = nums[i] - nums[j];
+            dp[i][diff] = dp[j].count(diff) ? dp[j][diff] + 1 : 2;
+            longest = max(longest, dp[i][diff]);
         }
     }
-    return ans;
-}
 
+    return longest;
+}
 
 int main(){
     int arr[] = {1,7,10,13,14,19},
             n = 6;
 
-    cout<<LengthOfLongestAP(arr,n);
+    cout<<lengthOfLongestAP(arr,n);  // 1 7 13 19
 }
