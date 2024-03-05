4a19165ff61affa959f611b3b8ed88be14d3f9e2
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-03-05 22:52:51 +0530

    Update Nodes_At_Distance_k.cpp

--- Binary_Tree/Nodes_At_Distance_k.cpp
+++ Binary_Tree/Nodes_At_Distance_k.cpp
@@ -1,36 +1,42 @@
+// leetcode 863: Store All Nodes Distance K from given node in Binary Tree
 #include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<set>
 using namespace std;
 
-class node{
+class TreeNode{
     public: 
-    int data;
-    node* left;
-    node* right;
+    int val;
+    TreeNode* left;
+    TreeNode* right;
 
-    node(int val){
-        this->data = val;
+    TreeNode(int val){
+        this->val = val;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
+// sol 1
+vector<int> ans;
 // case 1 : find nodes in bellow node(i.e. in subtree)
-void printSubtreeNodes(node *root, int k){
+void printSubtreeNodes(TreeNode *root, int k){
     if( root == NULL || k<0)
         return ;
-    
+
     if(k == 0){
-        cout<<root->data<<" ";
+        ans.push_back(root->val);
         return;
     }
-     
+
     // if (k>0)
     printSubtreeNodes(root->left, k-1); 
     printSubtreeNodes(root->right, k-1); 
 }
 
 // case 2 : find nodes in upper(or upper left or right) node(i.e. in ancestor)
-int printNodes_At_k(node *root, node *target, int k){
+int printNodes_At_k(TreeNode *root, TreeNode *target, int k){
     if(root == NULL)
         return -1;
 
@@ -43,7 +49,7 @@ int printNodes_At_k(node *root, node *target, int k){
     int dis_left = printNodes_At_k(root->left, target, k);
     if(dis_left != -1){
         if(dis_left + 1 == k)
-            cout<<root->data<<" ";
+            ans.push_back(root->val);
         else
             printSubtreeNodes(root->right, k-dis_left-2);
 
@@ -54,7 +60,7 @@ int printNodes_At_k(node *root, node *target, int k){
     int dis_right = printNodes_At_k(root->right, target, k);
     if(dis_right != -1){
         if(dis_right + 1 == k)
-            cout<<root->data<<" ";
+            ans.push_back(root->val);
         else
             printSubtreeNodes(root->left, k-dis_right-2);
 
@@ -64,8 +70,51 @@ int printNodes_At_k(node *root, node *target, int k){
     return -1;
 }
 
+vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+    printNodes_At_k(root, target, k);
+    return ans;
+}
+
+// sol 2
+// creating nodeToParent Map
+void populateMap(TreeNode *curr, TreeNode *parent, unordered_map<TreeNode*,TreeNode*> &nodeToParentMap){
+    if(!curr)
+        return;
+    nodeToParentMap[curr] = parent;
+    populateMap(curr->left,curr,nodeToParentMap);
+    populateMap(curr->right,curr,nodeToParentMap);
+}
+
+// finding all nodes at distance k
+void printAtKDistance(TreeNode *curr, int k, set<TreeNode*> &s,
+    unordered_map<TreeNode*, TreeNode*> nodeToParentMap, vector<int> &ans)
+{
+    if(!curr || s.find(curr)!=s.end() || k<0)
+        return;
+    s.insert(curr);
+    if(k==0){
+        ans.push_back(curr->val);
+        return;
+    }
+    printAtKDistance(curr->left,k-1,s,nodeToParentMap,ans);//call left child
+    printAtKDistance(curr->right,k-1,s,nodeToParentMap,ans);//call right child
+    printAtKDistance(nodeToParentMap[curr],k-1,s,nodeToParentMap,ans);//call parent
+    return;
+}
+
+vector<int> distanceK_2(TreeNode* root, TreeNode* target, int k) {
+    vector<int> res;
+    set<TreeNode*> s;
+    unordered_map<TreeNode*,TreeNode*> nodeToParentMap;
+    populateMap(root,NULL,nodeToParentMap);
+    printAtKDistance(target,k,s,nodeToParentMap,res);
+    return res;
+}
+
+
+
 int main(){
-    node* root = NULL;
+    TreeNode* root = NULL;
     // creating a tree
 /*        1
         /   \
@@ -75,16 +124,25 @@ int main(){
         \
         8
 */
-    root = new node(1);
-    root->left = new node(2);
-    root->right = new node(3);
-    root->left->left = new node(4);
-    root->left->right = new node(5);
-    root->right->left = new node(6);
-    root->right->right = new node(7);
-    root->left->right->right = new node(8);
+    root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    root->left->right->right = new TreeNode(8);
     
     
-    printNodes_At_k(root, root->left, 2);
+    vector<int> ans = distanceK(root, root->left, 2);
+    for(auto i: ans)
+        cout<<i<<" ";
+        
+    cout<<endl;
+
+    vector<int> res = distanceK_2(root, root->left, 2);
+    for(auto i: res)
+        cout<<i<<" ";
+
     return 0;
 }
