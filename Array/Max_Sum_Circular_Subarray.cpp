2b2a0f67f7eab1e575c80ef0f4e932adc65e3c2a
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-03-07 23:42:48 +0530

    Update Max_Sum_Circular_Subarray.cpp

--- Array/Max_Sum_Circular_Subarray.cpp
+++ Array/Max_Sum_Circular_Subarray.cpp
@@ -1,3 +1,6 @@
+// GFG Question : Hard
+// Given an array arr[] of N integers arranged in a circular fashion. 
+// Your task is to find the maximum contiguous subarray sum.
 #include<bits/stdc++.h>
 #include<climits>
 
@@ -19,8 +22,35 @@ int kadane(int arr[], int size){
     return maxSum;
 }
 
+//Function to find maximum circular subarray sum.
+int circularSubarraySum(int arr[], int size){
+    bool allNeg = true;
+    int maxi = INT_MIN;
+    for(int i=0; i<size; i++){
+        if(arr[i] >= 0)
+            allNeg = false;
+        maxi = max(maxi, arr[i]);
+    }
+    if(allNeg)
+        return maxi;
+    
+    int wrapsum, non_wrapsum;
+
+    non_wrapsum = kadane(arr,size);
+    int totalSum = 0;
+    for(int i=0; i<size; i++){
+        totalSum += arr[i];
+        arr[i] =-arr[i];
+    }
+
+    wrapsum = totalSum + kadane(arr, size);
+    // cout<<non_wrapsum<<" "<<wrapsum<<endl;
+    return max(wrapsum, non_wrapsum);
+
+}
+
 int main(){
-    int size, i, j, k;
+    int size, i;
     cout<<"Enter the size of array: ";
     cin>>size;
 
@@ -30,17 +60,7 @@ int main(){
         cin>>arr[i];
     }
 
-    int wrapsum, non_wrapsum;
-
-    non_wrapsum = kadane(arr,size);
-    int totalSum = 0;
-    for(i=0; i<size; i++){
-        totalSum += arr[i];
-        arr[i] =-arr[i];
-    }
-
-    wrapsum = totalSum + kadane(arr, size);
-    cout<<max(wrapsum, non_wrapsum);
+    cout<<"Maximum sum in circular subarray: "<<circularSubarraySum(arr,size);
     
     return 0;
 }
