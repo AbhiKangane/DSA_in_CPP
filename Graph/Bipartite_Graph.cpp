e611509808262bc5fe2bc0032f66d20a2c48e6ba
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-09-07 17:58:22 +0530

    Update Bipartite_Graph.cpp

--- Graph/Bipartite_Graph.cpp
+++ Graph/Bipartite_Graph.cpp
@@ -1,4 +1,4 @@
-// Bipartite Graphs: 
+// Bipartite Graphs: https://leetcode.com/problems/is-graph-bipartite/
 // 1. Its vertices can be divided into 2 disjoint & independent sets U & V such that every edge connects a vertex in U to  one in V
 // 2. Graph doesn't contain any odd length cycles
 // 3. The graph is 2 colorable
@@ -6,47 +6,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> adj;
-vector<bool> vis;
-vector<int> col;
-bool biPart = true;
+bool checkBFS(vector<vector<int>> &adj, int node, vector<int>&color, int currColor){
+    queue<int> q;
+    q.push(node);
+    color[node] = currColor;
 
-void color(int u, int curr){
-    if(col[u] != -1 and col[u] != curr){   // if vertex u is same colored with neighbour vertex or connected vertex
-        biPart = false;
-        return;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+
+        for(int &v : adj[u]){
+            if(color[v] == color[u])
+                return false;
+            else if(color[v] == -1){ // never colored or visited
+                color[v] = 1 - color[u]; // if color[u]=1,0 => color[v]=0,1
+                q.push(v);
+            }
+        }
     }
-    col[u] = curr;                        // giving color to vertex 
-    if(vis[u])                            // if vertex u already visited
-        return;
-    vis[u] = true;
-    for(auto i: adj[u]){
-        color(i, curr ^ 1);               // checking for all vertex
+    return true;
+}
+
+bool isBipartite(vector<vector<int>>& adj) {
+    int V = adj.size();
+    vector<int> color(V,-1);  // all vertices are uncolored / unvisited
+
+    // red:1, green:0
+    for(int i=0; i<V; i++){
+        if(color[i] == -1){
+            if(!checkBFS(adj,i,color,1))
+                return false;
+        }
     }
+    return true;
 }
 
 int main(){
-    int n,m;
-    cout<<"Enter no. of Edges & Vertices : ";
-    cin>>n>>m;
+    // not bipartite
+    vector<vector<int>> adj1 = {{1,2,3},{0,2},{0,1,3},{0,2}};
+    // bipartite
+    vector<vector<int>> adj2 = {{1,3},{0,2},{1,3},{0,2}};
 
-    adj = vector<vector<int>>(n);
-    vis = vector<bool>(n,false); 
-    col = vector<int>(n,-1); 
-
-    for (int i = 0; i < m; i++){
-        int u,v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);     // graph is directed
-    }
-
-    for(int i=0; i<n; i++){
-        if(!vis[i]){
-            color(i,0);
-        }
-    }
-    if(biPart)
+    if(isBipartite(adj2))
         cout<<"Graph is bipartite"<<endl;
     else
         cout<<"Graph is not bipartite"<<endl;
