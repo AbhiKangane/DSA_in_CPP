ace87e7600cbd933076450d272cbe10724284035
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-11-20 21:18:26 +0530

    Update

--- Array/Max_Sum_Subarray.cpp
+++ Array/Max_Sum_Subarray.cpp
@@ -1,32 +1,32 @@
+// Find the maximum sum of continuous subarray
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int size, i, j, k;
-    // cout<<"Enter the size of array: ";
-    // cin>>size;
+    int size;
+    cout<<"Enter the size of array: ";
+    cin>>size;
 
-    // int arr[size];
-    // cout<<"Enter the numbers in array: ";
-    // for(int i=0; i<size; i++){
-    //     cin>>arr[i];
-    // }
-
-    int arr[] = {-3, -5, -4, -2};
+    vector<int> arr(size);
+    cout<<"Enter the numbers in array: ";
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
 
 //  1st method : brute force
     // int maxSum = INT_MIN;
-    // for(i=0; i<size; i++)
-    //     for(j=i; j<size; j++){
+    // for(int i=0; i<size; i++)
+    //     for(int j=i; j<size; j++){
     //         int sum = 0;
-    //         for(k=i; k<=j; k++){
+    //         for(int k=i; k<=j; k++){
     //             sum += arr[k];
     //         }
     //         maxSum= max(maxSum, sum);
     //     }
     // cout<<maxSum;
 
-// 2nd method: Cumulative Sum approach
+// 2nd method: Cumulative Sum or Prefix Sum approach
     // int currsum[size+1];
     // currsum[0]= 0;
 
@@ -35,9 +35,9 @@ int main(){
     // }
 
     // int maxSum = INT_MIN;
-    // for(i=1; i<=size; i++){
+    // for(int i=1; i<=size; i++){
     //     int sum = 0;
-    //     for(j=0; j<i; j++){
+    //     for(int j=0; j<i; j++){
     //         sum = currsum[i] - currsum[j];
     //         maxSum = max(sum, maxSum);
     //     }
@@ -45,12 +45,19 @@ int main(){
     // cout<<maxSum;
 
 // 3rd Method: Kadane algorithm
-    int currentSum  = arr[0];
+    int currentSum  = 0;
     int maxSum = arr[0];
 
-    for(i=0; i<4; i++){
-        currentSum=max(arr[i],currentSum+arr[i]);
+    for(int i=0; i<size; i++){
+        // sol 1
+        currentSum += arr[i];
         maxSum = max(maxSum, currentSum);
+        if(currentSum < 0)
+            currentSum = 0;
+
+         // sol 2
+        // currentSum=max(arr[i],currentSum+arr[i]);
+        // maxSum = max(maxSum, currentSum);
     }
     
     std::cout<<maxSum<<endl;
