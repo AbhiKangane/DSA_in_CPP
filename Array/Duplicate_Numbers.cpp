809354759795049cbba18699765b6b24335f76ca
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-11-20 20:25:07 +0530

    Update

--- Array/Duplicate_Numbers.cpp
+++ Array/Duplicate_Numbers.cpp
@@ -1,107 +1,62 @@
+// https://www.geeksforgeeks.org/problems/duplicate-elements/1
 #include <bits/stdc++.h>
 using namespace std;
 
-void printArray(int *A, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-}
+// O(n*log(n))
+vector<int> DuplicateNumbers1(vector<int>arr, int n){
+    sort(arr.begin(), arr.end());
 
-int maximum(int a[], int size){
-    int max=a[0];
-    for(int i=0; i< size; i++){
-        if(a[i] > max)
-            max= a[i];
-    }
-    return max;
-}
+    vector<int> ans;
 
-bool containsDuplicate(int* a, int size){
-    int max = 10000;
-    int ind[max+1], i;
-    for(i=0;i<=max;i++)
-        ind[i] = 0;
-    
-    for(i=0;i<size;i++){
-        ind[a[i]]++;
-    }
-    
-    // cout<<"Duplicate numbers are: ";
-    for(i=0;i<=max;i++){
-        if(ind[i] > 1){
-            // cout<<i<<" is appearing "<<a[i]<<" times"<<endl;
-            return true;
+    for(int i=0; i<n-1; i++) {
+        if(arr[i] == arr[i+1]) {
+            ans.push_back(arr[i]);
+            while(i<n-1 && arr[i] == arr[i+1]) i++; // Skip all duplicates
         }
     }
-    return false;
+
+    return ans;
+}
+
+// O(n)
+vector<int> DuplicateNumbers2(vector<int>arr, int n){
+    unordered_map<int,int> mp;
+    vector<int> ans;
+
+    for(auto i: arr){
+        mp[i]++;
+    }
+
+    for(auto i: mp){
+        // cout<<i.first<<" "<<i.second<<endl;
+        if(i.second > 1)
+            ans.push_back(i.first);
+    }
+
+    return ans;
 }
 
 int main(){
-    int i=0, j=0, size, count;
+    int n;
     cout<<"Enter number of elements in array: ";
-    cin>>size;
+    cin>>n;
 
-    int a[size];
+    vector<int> arr(n);
     cout<<"Enter numbers in array: ";
-    for(i=0; i<size; i++)
-        cin>>a[i];
-     
-    //  method 1: it requires sorted array
- /*   int lastDuplicate = 0;
-    for(int i=0; i<size; i++){
-        if(a[i]==a[i+1] && a[i] != lastDuplicate){
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
 
-            for(j=i+1; a[i]==a[j]; j++){}
-            
-            cout<<a[i]<<" is appearing "<<j-i<<" times\n";
-            i=j-1;
-            
-            // cout<<a[i]<<" ";
-            // lastDuplicate = a[i];
-        }
-    }
-*/
-
-// Method 2 also known as hashing techinque: this doesn't need sorted array
-/*    int max = maximum(a, size);
-    int ind[max+1];
-    for(i=0;i<=max;i++)
-        ind[i] = 0;
+    vector<int> ans = DuplicateNumbers1(arr,n);
+    sort(ans.begin(), ans.end());
     
-    for(i=0;i<size;i++){
-        ind[a[i]]++;
-    }
-    
-    cout<<"Duplicate numbers are: ";
-    for(i=0;i<=max;i++){
-        if(ind[i] > 1)
-            cout<<i<<" is appearing "<<a[i]<<" times"<<endl;
+    if(ans.size()>0){
+        cout<<"Duplicated no. in array are: ";
+        for(auto i:ans)
+            cout<<i<<" ";    
     }
 
-    // cout<<endl;
-    // printArray(ind, max+1);
-*/
-
-    cout<<containsDuplicate(a,size);
-
-// Method 3: Time complexity = O(n^2)
-    // for(i=0; i<size-1; i++){
-    //     count = 1;
-    //     if(a[i] != -1){
-    //         for(j=i+1; j<size; j++){
-    //         if(a[i] == a[j]){
-    //             count++;
-    //             a[j] = -1;
-    //         }
-    //     }
-    //     if(count>1)
-    //             cout<<"Number "<<a[i]<<" is duplicates "<<count<<" times"<<endl;
-    //     }
-    // }
+    else
+        cout<<"No Duplicated number is present ";
 
     return 0;
 }
-    
