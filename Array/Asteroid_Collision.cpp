be7f1d70cc28d8c16e3c985dc6d5710247193f70
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-11-21 22:16:45 +0530

    Update

--- Array/Asteroid_Collision.cpp
+++ Array/Asteroid_Collision.cpp
@@ -1,3 +1,9 @@
+// GFG: https://www.geeksforgeeks.org/problems/asteroid-collision/1
+// Problem Statement : We are given an integer array asteroids of size N representing asteroids in a row. For each asteroid, the absolute value represents its size & 
+// the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+// Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. 
+// If both are of same size, both will explode. Two asteroids moving in the same direction will never meet.
+
 // +ve => right direction
 // -ve => left direction
 // Only considered magnitude
