76e308b83fd20cd2deff1d48c4c15ca31df287db
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-11-21 22:30:43 +0530

    Update

--- Array/Smallest_Positive_Missing_No.cpp
+++ Array/Smallest_Positive_Missing_No.cpp
@@ -1,20 +1,21 @@
+// Print smallest positive missing number from given array
+// ex: arr={-1,3,0,-4,5,1}
+// output: 2
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int size, i;
+    int n, i;
     cout<<"Enter size of array: ";
     cin>>size;
 
-    int arr[size];
+    vector<int> arr(n);
     cout<<"Enter numbers in array: ";
-    for(i=0; i<size; i++)
+    for(i=0; i<n; i++)
         cin>>arr[i];
 
-    int max = 100;
-    bool check[max];
-    for(i=0; i<max; i++)
-        check[i] = false;
+    int max = *max_element(arr.begin(), arr.end());;
+    vector<bool> check(max);
 
     for(i=0; i<size; i++){
         if(arr[i] >= 0){
