a780252a13c6384d4ef866986c2acae08b1a3153
Abhishek Kangane <123063938+AbhiKangane@users.noreply.github.com>
2024-12-30 22:39:59 +0530

    Update

--- TCS/TCS_TAG_8_Mar_24/No_Of_Ways.cpp
+++ TCS/TCS_TAG_8_Mar_24/No_Of_Ways.cpp
@@ -18,7 +18,23 @@ Explaination : GYR, GRY, RGY, RYG, YGR, YRG
 #include<algorithm>
 using namespace std;
  
-// Storing  unique permutations 
+// sol 1: using recusrion
+int solveRec(int R, int Y, int G, bool r, bool y, bool g){
+    if(R==0 && Y==0 && G==0)
+        return 1;
+    
+    else if(R==-1 || Y==-1 || G==-1)
+        return 0;
+
+    int count = 0;
+    if(r)       count += solveRec(R-1,Y,G,0,1,1);
+    if(y)       count += solveRec(R,Y-1,G,1,0,1);
+    if(g)       count += solveRec(R,Y,G-1,1,1,0);
+
+    return count;
+}
+
+// sol 2: Storing  unique permutations 
 void FindUniquePerm(int left, int right, string &s, set<string> &ans){
     if (left == right){// reach the end of s
         ans.insert(s);
@@ -44,8 +60,11 @@ vector<string> permuteUnique(string &s) {
 
 int main(){
     int g, y, r;
-    cout<<"Enter no of greent, yellow, red balls: ";
+    cout<<"Enter no of green, yellow, red balls: ";
     cin>>g>>y>>r;
+
+    cout<<"\nNo of ways to arrange balls by recursion only: "<<solveRec(g,y,r,1,1,1)<<endl;
+
     string str ="";
     for(int i=0; i<g; i++)
         str+="G";
@@ -70,7 +89,7 @@ int main(){
             }
         }
     }
-    
+    // this solution gives TLE
     cout<<"\nNo of ways to arrange balls: "<<ans<<endl;
 
     return 0;
